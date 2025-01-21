b4916161e8249e73014ba1719ac40cc39d9ec9a1
LeeSangMin0329 <329lsm@gmail.com>
2025-01-22 04:56:45 +0900

    support stt with whisper model

--- openai_controller.py
+++ openai_controller.py
@@ -1,3 +1,5 @@
+import numpy as np
+import io
 import openai
 from openai_key import API_KEY
 
@@ -11,3 +13,15 @@ def query_gpt(messages: list) -> str:
     
     return completion.choices[0].message.content
     
+def query_stt(audio: io.BytesIO, language="ja") -> str:
+    file_path = "./temp/temp_audio.mp3"
+    with open(file_path, "wb") as f:
+        f.write(audio.read())
+
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=open(file_path, "rb"),
+        language=language,
+        response_format="text"
+    )
+    return transcription.strip()
--- test_app.py
+++ test_app.py
@@ -1,9 +1,11 @@
 import requests
 import time
+import base64
+from io import BytesIO
 from flask import Flask, request, jsonify
 from threading import Thread
 from message_utils import extract_jp_message, remove_emoji
-from openai_controller import query_gpt
+from openai_controller import query_gpt, query_stt
 
 import db_manager
 import prompt_introduction
@@ -12,17 +14,41 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/chat", methods=["POST"])
-def chat():
+def receive_text():
     user_input = request.form.get("message")
 
     if not str(user_input).strip():
         return jsonify({"response": ""}) 
     
+    message_jp = chat(user_input)
+
+    return jsonify({"response": message_jp})
+
+@app.route("/chat_audio", methods=["POST"])
+def receive_audio():
+    audio = request.form.get("message")
+
+    decoded_data = base64.b64decode(audio)
+
+    audio_buffer = BytesIO(decoded_data)
+
+    user_input = query_stt(audio_buffer, language="ja")
+
+    if not user_input:
+        return
+
+    print(f"user: {user_input}")
+
+    message_jp = chat(user_input)
+
+    return jsonify({"response": message_jp})
+
+def chat(user_input: str):
     start = time.time()
     
     converstations = db_manager.get_conversations()
-    converstations.insert(0, prompt_introduction.rule_message)
-    converstations.insert(0, prompt_introduction.introduction_message)
+    converstations.insert(0, prompt_introduction.rule_message_jp)
+    converstations.insert(0, prompt_introduction.introduction_message_jp)
 
     current_time = datetime.now()
     timestamp = current_time.strftime(f"%Y-%m-%d %H:%M:%S")
@@ -42,18 +68,20 @@ def chat():
     db_manager.add_converstaion("user", user_input)
     db_manager.add_converstaion("assistant", message_jp, face, motion)
     
-    # thread = Thread(target=send_to_tts_server, args=(message_jp,))
-    # thread.start()
+    thread = Thread(target=send_to_tts_server, args=(message_jp, face, motion))
+    thread.start()
 
-    return jsonify({"response": message_jp})
+    return message_jp
 
-def send_to_tts_server(message: str):
+def send_to_tts_server(message: str, face: str, motion: str):
     print(f"jp message: {message}")
     try:
-        url = str("http://127.0.0.1:50001/requset_message_form_outside")
-        response = requests.post(url, data={'message': message})
+        url = str("http://127.0.0.1:50000/generate_audio")
+        response = requests.post(url, data={'message': message, 'face': face, 'anim': motion})
 
         if response.status_code == 200:
+            pass
+        elif response.status_code == 202:
             pass
         else:
             print(f"TTS server failed with status code: {response.status_code}")
@@ -66,14 +94,14 @@ def test_route(message: str):
         response = chat()
         print(response.get_json()['response'])  # 응답 출력
 
-#app.run(host="127.0.0.1", port="50003")
-def main():
-    while True:
-        message = input("Enter message: ")
-        test_route(message)
+app.run(host="127.0.0.1", port="50003")
+# def main():
+#     while True:
+#         message = input("Enter message: ")
+#         test_route(message)
 
-        if message is None:
-            continue
+#         if message is None:
+#             continue
         
         
-main()
+# main()
